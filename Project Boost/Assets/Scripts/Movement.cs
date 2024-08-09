07070a3e5fe70312422235a9ea8415a89b27b8e6
Utpal Kant Mishra <authorutpal@gmail.com>
2024-08-09 22:10:31 +0530

    Final Changes till 09.08.2024

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rb;    
+    
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         
     }
 
@@ -22,7 +28,7 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             
-            Debug.Log("Pressed Space - thrusting");
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);            
         }
         
     }
